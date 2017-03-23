1760924dc17b362647a77272f4e1f578ac2d7a3e
tophf <tophf@gmx.com>
2017-03-24 01:51:37 +0300

    fixup!

--- manage.js
+++ manage.js
@@ -233,13 +233,11 @@ class EntryOnClick {
 function handleUpdate(style, {reason} = {}) {
   const element = createStyleElement(style);
   const oldElement = $(`[style-id="${style.id}"]`, installed);
-  if (reason == 'update' || reason == 'editSave') {
-    element.addEventListener('animationend', function _() {
-      element.removeEventListener('animationend', _);
-      element.classList.remove('highlight');
-    });
-    element.classList.add('highlight');
-  }
+  element.addEventListener('animationend', function _() {
+    element.removeEventListener('animationend', _);
+    element.classList.remove('highlight');
+  });
+  element.classList.add('highlight');
   if (!oldElement) {
     installed.appendChild(element);
   } else {
@@ -249,8 +247,11 @@ function handleUpdate(style, {reason} = {}) {
       $('.update-note', element).innerHTML = t('updateCompleted');
     }
   }
-  // align to the bottom of the visible area if wasn't visible
-  element.scrollIntoView(false);
+  // align to the top/bottom of the visible area if wasn't visible
+  const bounds = element.getBoundingClientRect();
+  if (bounds.top < 0 || bounds.top > innerHeight - bounds.height) {
+    element.scrollIntoView(bounds.top < 0 );
+  }
 }
 
 
